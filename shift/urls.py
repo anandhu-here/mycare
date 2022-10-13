1ad065dc8f28e9b1dca80191b23c6c06127b4ed3
Anandhu Satheesh <anandhuuhere@gmail.com>
2022-10-13 23:45:48 +0100

    initial commit

--- shift/urls.py
+++ shift/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .api import assign_shifts, complete_shift, get_shifts, publish_shifts
+from .api import assign_shifts, complete_shift, get_avail, get_shifts, post_avail, publish_shifts
 
 urlpatterns=[
     path('shifts', get_shifts),
     path('assign-shifts', assign_shifts),
     path('publish-shifts', publish_shifts),
-    path('complete-shift', complete_shift)
+    path('complete-shift', complete_shift),
+    path('get-avail', get_avail),
+    path('post-avail', post_avail)
 ]
